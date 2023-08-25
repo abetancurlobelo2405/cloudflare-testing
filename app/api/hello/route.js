9c923a4467d4bc93977dd1e4a92917a507df391d
andres81 <andres_242001@hotmail.com>
2023-08-24 21:09:14 -0500

    v1

--- app/api/hello/route.js
+++ app/api/hello/route.js
@@ -1,3 +1,7 @@
+export const runtime = "edge";
+
 export async function GET(request) {
-  return new Response('Hello, Next.js!')
+  return new Response("Desde la api de hello!!!!", {
+    status: 200,
+  });
 }
